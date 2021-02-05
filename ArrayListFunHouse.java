8257b8e58d3573d7d3e9fcb166c33316bcc3ac96
Nathan Chapero <600020076@fjuhsd.org>
2021-02-05 17:24:45 +0000

    updated the run button file.

--- ArrayListFunHouse.java
+++ ArrayListFunHouse.java
@@ -12,7 +12,23 @@ public class ArrayListFunHouse
 {
  public static ArrayList<Integer> getListOfFactors(int number)
  {
-  return null;
+  /*
+ //create a for loop that initializes integer i = 2. It will run through every integer until it reaches the parameter number.
+ for(int i = 2; i < number; i++)
+ {
+   //create an if loop that will check if the remainder is 0 when number is divided by i.
+   if(number % i == 0)
+   {
+     //if the remainder is zero, then i will be added to the arrayList.
+     ArrayListFunHouse.add(i);
+   }
+//continue until all values have been tested
+
+ }
+//exit the if and for loop and print the final arrayList.
+System.out.println(ArrayListFunHouse());
+*/
+return null;
  }
  
  public static void keepOnlyCompositeNumbers( List<Integer> nums )
